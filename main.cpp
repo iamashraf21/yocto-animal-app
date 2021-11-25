7a527d9d0a4d38da60b945aa699ea2099ea1cf3c
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-11-25 18:41:53 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,16 @@
 using namespace std;
 
 #include "animal.h"
+#include "dog.hpp"
 
 int main()
 {
-    Animal a1("Monkey");
-    cout<<a1.getSpecies()<<endl;
+    // Animal a1("Monkey");
+    // cout<<a1.getSpecies()<<endl;
+    Dog d1("Tuffy", 5);
+    cout<<d1.getDogName()<<endl;
+    cout<<d1.getDogAge()<<endl;
+
 
     return 0;
 }
