0a8c54485dcde960ba5412b9b064960e5ea0ee93
iamashraf21 <iamashraf21@gmail.com>
2021-11-25 14:11:21 +0530

    first commit

---
+++ animal.cpp
@@ -0,0 +1,16 @@
+#include "animal.h"  
+
+Animal::Animal()
+{
+    cout<<"Deafult";
+}
+
+Animal::Animal(const string& name)
+{
+    species = name;
+}
+
+string Animal::getSpecies()
+{
+    return species;
+}
